63d0cae1094533aaf874c39889cd323166c229df
Daniil <39118675+TyPaporotnyk@users.noreply.github.com>
2024-03-01 08:11:54 +0200

    added logging to auction

--- src/auction/__main__.py
+++ src/auction/__main__.py
@@ -1,20 +1,25 @@
 import asyncio
 from datetime import datetime, time
 
+from loguru import logger
+
 from src.auction.find import find_auction_authors, find_private_auction_authors
 
 
 async def main():
+    logger.info("Start auction")
     start_time = time(8, 0)
     end_time = time(22, 0)
     while True:
         now = datetime.now().time()
 
         if start_time < now < end_time:
+            logger.info("Start auction sennding")
             await find_auction_authors()
             await find_private_auction_authors()
 
         else:
+            logger.info(f"Time is not reachble: {now}")
             await asyncio.sleep(3600)
             continue
 
