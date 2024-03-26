dd5f9c5278dbeccdd7f6660515212e46a023e90d
Daniil <39118675+TyPaporotnyk@users.noreply.github.com>
2024-03-26 14:26:06 +0200

    Added some changes

--- src/bot/handlers/__init__.py
+++ src/bot/handlers/__init__.py
@@ -3,14 +3,12 @@ from loguru import logger
 
 from .admin import router as admin_router
 from .author import router as author_router
-from .base import router as base_router
 
 
 def setup(dp: Dispatcher):
     """Setting up handlers"""
     logger.info("Settng up routers")
 
-    dp.include_router(base_router)
     dp.include_router(author_router)
     dp.include_router(admin_router)
 
