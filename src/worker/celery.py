9cdc789f6cc97a6f5451ac737da08799e3b99b1d
Daniil <daniil.svadkovsky@gmail.com>
2024-04-13 00:05:28 +0300

    Created admin alert task

--- src/worker/celery.py
+++ src/worker/celery.py
@@ -10,6 +10,10 @@ app.conf.beat_schedule = {
         "task": "src.worker.tasks.dump_crm_data_task",
         "schedule": crontab(minute="*/5"),
     },
+    "send-team-leads-alerts-task": {
+        "task": "src.worker.tasks.send_team_leads_alerts_task",
+        "schedule": crontab(minute="0", hour="10"),
+    },
 }
 app.conf.beat_schedule_filename = "data/celerybeat-schedule"
 app.conf.update(broker_connection_retry_on_startup=True)
--- src/worker/tasks.py
+++ src/worker/tasks.py
@@ -2,11 +2,12 @@ import asyncio
 import logging
 from typing import List
 
+from src.alert.admin import send_team_leads_alerts
 from src.auction.find import find_auction_authors, find_private_auction_authors
 from src.bot.misc import bot
 from src.bot.services.broadcaster import broadcast
-from src.worker.celery import app
 from src.dump import dump
+from src.worker.celery import app
 
 logger = logging.getLogger(__name__)
 
@@ -33,3 +34,9 @@ def find_auction_authors_task():
 def search_authors_for_private_auction():
     celery_event_loop = asyncio.get_event_loop()
     celery_event_loop.run_until_complete(find_private_auction_authors())
+
+
+@app.task
+def send_team_leads_alerts_task():
+    celery_event_loop = asyncio.get_event_loop()
+    celery_event_loop.run_until_complete(send_team_leads_alerts())
