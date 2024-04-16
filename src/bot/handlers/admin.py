47ecd90bc520cdbb4b85ae1b69f31efe4133e72e
Daniil <daniil.svadkovsky@gmail.com>
2024-04-16 16:46:05 +0300

    Updated urgent list handler

--- src/bot/handlers/admin.py
+++ src/bot/handlers/admin.py
@@ -12,11 +12,11 @@ from src.db import Lead
 from src.db.services.admin import get_admin_author_ids, get_admin_authors
 from src.db.services.author import create_base_author
 from src.db.services.lead import (
+    get_admin_salary,
     get_author_payment_list,
     get_current_author_payments,
     get_current_author_tasks,
     get_urgent_list,
-    get_admin_salary,
 )
 from src.worker import send_user_messages_task
 
@@ -155,19 +155,17 @@ async def show_urgent_list(callback: types.CallbackQuery, session):
     """
     Show the urgent list of the authors
     """
-    admin_id = callback.from_user.id
+    # admin_id = callback.from_user.id
+    admin_id = 6414134198
     urgent_list = await get_urgent_list(session, admin_id)
 
     urgent_list_typed = get_typed_leads(urgent_list)
 
     for urgent_status in urgent_list_typed:
-        message = ""
+        message = f"Список термінових робіт за статусом {urgent_status}:\n"
 
         for urgent in urgent_list_typed[urgent_status]:
-            message += (
-                f"{urgent.id}; {urgent.deadline_for_author.strftime('%Y-%m-%d')};"
-                f" {urgent.author_id} - {urgent.status}\n"
-            )
+            message += f"{urgent.id}; {urgent.deadline_for_author.strftime('%m-%d')}; {urgent.author_id}\n"
 
         await callback.message.answer(message)
 
